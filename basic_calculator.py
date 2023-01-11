20db8e243d72d48e323bb946432c150c150f1a46
Kamil Prokop <zara.pug.yt@gmail.com>
2023-01-11 01:24:22 +0100

    Init arithmetic specification

--- basic_calculator.py
+++ basic_calculator.py
@@ -9,3 +9,18 @@ types_of_calculations = {
     "3": ["Mnożenie", "czynnik", "czynnik", "Mnożę", "przez"],
     "4": ["Dzielenie", "dzielną", "dzielnik", "Dzielę", "przez"]
 }
+
+specification_order_first = specification_order_second = " "
+
+if (intro_question == "1" or intro_question == "3"):
+    specification_order_first = " 1"
+    specification_order_second = " 2"
+
+first_number_text = "Podaj " + types_of_calculations.get(intro_question)[1] + specification_order_first + ": "
+first_number = float(input(first_number_text))
+
+second_number_text = "Podaj " + types_of_calculations.get(intro_question)[2] + specification_order_second + ": "
+second_number = float(input(second_number_text))
+
+arithmetic_description = str(str(types_of_calculations.get(intro_question)[3]) + " " + str(first_number) + " " 
+                            + str(types_of_calculations.get(intro_question)[4]) + " " + str(second_number))
