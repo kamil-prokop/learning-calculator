658d7aba0590e43120bd0a1f624ee2fe8aa2a086
Kamil Prokop <zara.pug.yt@gmail.com>
2023-01-11 01:21:25 +0100

    Init arithmetic wording

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,2 +1,11 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
+
+intro_question = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+
+types_of_calculations = {
+    "1": ["Dodawanie", "składnik", "składnik", "Dodaję", "do"],
+    "2": ["Odejmowanie", "odjemną", "odjemnik", "Od", "odejmuję"],
+    "3": ["Mnożenie", "czynnik", "czynnik", "Mnożę", "przez"],
+    "4": ["Dzielenie", "dzielną", "dzielnik", "Dzielę", "przez"]
+}
